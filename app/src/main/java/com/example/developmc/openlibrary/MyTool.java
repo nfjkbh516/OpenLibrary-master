1a0e2e3be0d919f60b79009194e2dc1919dbeb34
陈建 <chenjian@kuai-info.com>
2016-11-30 10:57:06 +0800

    xiugai

--- app/src/main/java/com/example/developmc/openlibrary/MyTool.java
+++ app/src/main/java/com/example/developmc/openlibrary/MyTool.java
@@ -1,8 +1,18 @@
 package com.example.developmc.openlibrary;
 
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+
 /**
  * Created by developmc on 16/5/21.
  */
 public class MyTool {
-    public void myMethod(){}
+    public void myMethod(){
+
+
+    }
 }
